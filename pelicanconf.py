fb3274cd3fc8028bb397ae892f416c5ee8367e05
Bru <bruno.ronchi@muttdata.ai>
2021-08-07 15:07:23 -0300

    Updated "quiero un blog" post

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,10 +19,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Disclaimer', 'disclaimer'),)
 
 # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
